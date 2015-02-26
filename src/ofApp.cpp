cc1fdf94094633110dc83ba9c3f441d1e2869019
Kyoko Kuroda <ancooo@ancooo.com>
2015-02-26 19:37:27 +0900

    adjust appearance

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,22 +8,22 @@ void ofApp::setup(){
     
     // Box2Dの世界の設定
     box2d.init();               // 初期化
-    box2d.setGravity(0, 10);    // 重力
+    box2d.setGravity(0, 5);    // 重力
     box2d.createBounds();       // 画面の周囲に壁を作成
-    box2d.setFPS(30.0);         // box2Dの世界でのFPS
+    box2d.setFPS(60.60);         // box2Dの世界でのFPS
     box2d.registerGrabbing();   // 物体をつかめるようにする
     
     //  ground
     box2d.registerGrabbing();
     box2d.createGround(400, 100, 9000, 100);
-    groundLine.addVertex(100, 400);
-    groundLine.addVertex(540, 400);
+    //groundLine.addVertex(100, 400);
+    //groundLine.addVertex(540, 400);/Users/ancooo/Documents/of_v0.8.4_osx_release/apps/myApps/theta-viewer.js-master
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     box2d.update();             // box2Dの更新
-    float r = ofRandom(1, 70);
+    float r = ofRandom(1, 60);
     circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
     circles.back().get()->setPhysics(3.0, ofRandom(0,1.5), 360);
     circles.back().get()->setup(box2d.getWorld(), ofRandom(0,1024), ofRandom(-200,0), r);
@@ -36,7 +36,7 @@ void ofApp::draw(){
     groundLine.draw();
     
     // 円を描画
-    for(int i=0; i<circles.size(); i++) {
+    for(int i=10; i<circles.size(); i++) {
         ofFill();
         ofSetColor(ofRandom(1,255),ofRandom(1,255),ofRandom(1,255));
         circles[i].get()->draw();
